d5feb3a7ac04d9899e83ffb62e3c834af6631d7c
omar faruk <omarfaroke@gmail.com>
2021-03-05 17:10:50 +0300

    init 2

--- android/app/src/main/kotlin/com/example/food_preservation/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.food_preservation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/ui/theme/app_colors.dart
+++ lib/ui/theme/app_colors.dart
@@ -9,18 +9,18 @@ class AppColors {
   // static const Color textPrimary = Color(0xFF263238);
 
   //Colors for theme
-  static const Color lightPrimary = primary_2;
-  static const Color lightAccent = Colors.white;
+  static const Color lightPrimary = primary_1;
+  static const Color lightAccent = primary_3;
   static const Color lightTextAppBar = primary_3;
   static const Color lightTextPrimary = lightPrimary; 
   static const Color lightTextButton = lightAccent; 
   static const Color iconColor = primary_1;
   static const Color buttonColor = primary_2;
 
-  static const Color primary_1 = Color(0xFFf8940c);
-  static const Color primary_2 = Color(0xFF94c31c);
-  static const Color primary_3 = Color(0xFFdc9c14);
-  static const Color primary_4 = Color(0xFF0c6bf3);
+  static const Color primary_1 = Color(0xFFea1bc8);
+  static const Color primary_2 = Color(0xFFf4de1d);
+  static const Color primary_3 = Color(0xFFf8c6c8);
+  static const Color primary_4 = Color(0xFF67b3b5);
 
 
   
